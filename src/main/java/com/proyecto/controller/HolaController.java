78acfcd15710534ec442dcd97d937e8b14abd1d7
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-11-25 18:42:07 -0500

    Create a topicoController for pass a list of them

--- src/main/java/com/proyecto/controller/HolaController.java
+++ src/main/java/com/proyecto/controller/HolaController.java
@@ -1,13 +1,11 @@
 package com.proyecto.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HolaController {
 	
 	@RequestMapping("/")
-	@ResponseBody
 	public String Hola() { return "hola";}
 }
---
+++ src/main/java/com/proyecto/forum/model/StatusTopico.java
@@ -0,0 +1,7 @@
+package com.proyecto.forum.model;
+
+public enum StatusTopico {
+
+	SIN_RESPUESTA, NO_SOLUCIONADO, SOLUCIONADO, CERRADO;
+
+}

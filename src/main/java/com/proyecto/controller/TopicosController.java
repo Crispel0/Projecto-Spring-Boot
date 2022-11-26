7058a6644cfa6d52b290d1958f7d845763237310
Crispelo <97807364+Crispel0@users.noreply.github.com>
2022-11-25 22:02:34 -0500

    Add DevTools

---
+++ src/main/java/com/proyecto/controller/TopicosController.java
@@ -0,0 +1,29 @@
+package com.proyecto.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.forum.model.Curso;
+import com.proyecto.forum.model.Topico;
+
+@RestController //Create a ResponseBody into
+public class TopicosController {
+	
+	
+	/**
+	 * Create a object of topicos, pass your valuer for parameters and return a {@link Arrays} "static" to {@link List}
+	 */
+	@RequestMapping("/topicos")
+	List<Topico> listar (){
+		
+		Topico topico = new Topico("Titulo Topico", "Mensaje Topico", new Curso("Linux", "DevOps"));
+		Topico topico2 = new Topico("Titulo Topico", "Mensaje Topico", new Curso("C#", "Programacion"));
+		Topico topico3 = new Topico("Titulo Topico", "Mensaje topico", new Curso("JS", "Lenguajes Programacion"));
+		
+		return Arrays.asList(topico, topico2, topico3);
+		
+	}
+}
